2dee7c4453221c68a56cef670b3d31aa68cc6cd5
Evgeny Lahav <Evgeny_Lahav@amat.com>
2019-06-06 01:16:30 +0300

    fe functionality changes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,49 +1,44 @@
 export function parseRowsLocation(dataIn){
-    const rowSpacing = 100;
-    const rows1 = Array(dataIn.nodes.length);
-    let rows = []
-  
-    for (let i = 0; i< rows1.length; i++) {
-      rows[i] = (i+1)*rowSpacing;
-    }
-  
-    // console.log(`rows: ${rows}`);
-    return rows
+    return rangeArray(100, 100, dataIn.nodes.length)
 }
 
 export function parseColLocation(dataIn){
-    const colSpacing = 100;
-    const cols1 = Array(dataIn.all_user_names.length + 1);
-    let cols = []
+    return rangeArray(100, 100, dataIn.all_user_names.length + 1)
+}
+
+function rangeArray(start, spacing, size) {
+    let arr = []
   
-    for (let i = 0; i< cols1.length; i++) {
-      cols[i] = (i+5)*colSpacing;
+    for (let i = 0; i < size; i++) {
+      arr[i] = (i+1) * spacing;
     }
-    // console.log(`cols: ${cols}`);
-    return cols;
+    return arr;
+
 }
 
 export function parseUsersToColumns(dataIn, cols) {
+    let data = {...dataIn};
     let usersToColumns = {};
-    dataIn.all_user_names = dataIn.all_user_names.concat(['Messages']);
-    dataIn.all_user_names.forEach((key, i) => usersToColumns[key] = cols[i]);
+    data.all_user_names = data.all_user_names.concat(['Messages']);
+    data.all_user_names.forEach((key, i) => usersToColumns[key] = cols[i]);
 
     return usersToColumns;
 }
 
 export function parseArrows(dataIn, usersToColumns, rows) {
     let arrows = [];
-    let i = 0;
-    for (i ; i < dataIn.edges.length; i++) {
+
+    for (let i=0; i < dataIn.edges.length; i++) {
       
       const user_name_1 = dataIn.nodes.find( node => node.id === dataIn.edges[i][0] ).user_name;
       const user_name_2 = dataIn.nodes.find( node => node.id === dataIn.edges[i][1] ).user_name;
 
-      arrows[i] = {};
-      arrows[i].x1 = usersToColumns[user_name_1];
-      arrows[i].y1 = rows[dataIn.edges[i][0]-1];
-      arrows[i].x2 = usersToColumns[user_name_2];
-      arrows[i].y2 = rows[dataIn.edges[i][1]-1];
+      arrows.push({
+          x1: usersToColumns[user_name_1],
+          y1: rows[dataIn.edges[i][0]-1],
+          x2: usersToColumns[user_name_2],
+          y2: rows[dataIn.edges[i][1]-1],
+      });
     }
 
     return arrows;
