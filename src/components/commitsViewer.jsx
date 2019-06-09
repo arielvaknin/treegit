6a98275a670ec2882268f0c951d616496611cec2
Niv Koren <Niv_Koren@amat.com>
2019-06-09 20:56:37 +0300

    disable full commit window befor selecting any commit niv

--- src/components/commitsViewer.jsx
+++ src/components/commitsViewer.jsx
@@ -4,28 +4,38 @@ import { Alert } from 'react-bootstrap';
 export class CommitViewer extends React.Component {
   render() {   
     const commitData = this.props.commitData;
-    return (
-      <Alert variant="success" style={{marginTop: '10px'}}>
-        <Alert.Heading>Details for Commit {commitData.id}</Alert.Heading>
-        <p>
-          Commit user name is: <u>{commitData.user_name}</u>
-        </p>
-        <p>
-          Commit date is: <u>{commitData.commit_date}</u>
-        </p>
-        <p>
-          Commit message is: <u>{commitData.commit_msg}</u>
-        </p>
-        <hr />
-        <p className="mb-0">
-          Commit hash code is: <br /> {commitData.hash_key}
-        </p>
-        <br />
-        <p>
-          Commit parent/s is/are: <br />
-          { commitData.parents.map( item => item + "\n") }
-        </p>
-      </Alert>
-    );
+    if (commitData.id == 0){
+      return (
+        <Alert variant="success" style={{marginTop: '10px'}}>
+          <p>
+            <u>Select commit to watch details</u>
+          </p>
+        </Alert>
+      ) 
+    } else {
+      return (
+        <Alert variant="success" style={{marginTop: '10px'}}>
+          <Alert.Heading>Details for Commit {commitData.id}</Alert.Heading>
+          <p>
+            Commit user name is: <u>{commitData.user_name}</u>
+          </p>
+          <p>
+            Commit date is: <u>{commitData.commit_date}</u>
+          </p>
+          <p>
+            Commit message is: <u>{commitData.commit_msg}</u>
+          </p>
+          <hr />
+          <p className="mb-0">
+            Commit hash code is: <br /> {commitData.hash_key}
+          </p>
+          <br />
+          <p>
+            Commit parent/s is/are: <br />
+            { commitData.parents.map( item => item + "\n") }
+          </p>
+        </Alert>
+      );
+    }
   }
 }
